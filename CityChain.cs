a384e6525b9a9b6c5da5ff5805c67d7bdf8ca704
pershakova <pershakova@bk.ru>
2016-11-02 02:25:17 +0300

    city-chain test project

---
+++ CityChain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CityChain
+{
+    public class CityChain
+    {
+        public Dictionary<string, string> CreateChain(string[,] cities)
+        {
+            Dictionary<string, string> firstCityKeyDict = new Dictionary<string, string>();
+            Dictionary<string, string> secondCityKeyDict = new Dictionary<string, string>();
+            Dictionary<string, string> res = new Dictionary<string, string>();
+
+            for (int i = 0; i < cities.GetLength(0); i++)
+            {
+                firstCityKeyDict.Add(cities[i, 0], cities[i, 1]);
+                secondCityKeyDict.Add(cities[i, 1], cities[i, 0]);
+            }
+
+            string nextElement = string.Empty;
+            foreach (KeyValuePair<string, string> kvp in firstCityKeyDict)
+            {
+                if (!secondCityKeyDict.ContainsKey(kvp.Key))
+                {
+                    //find the first element
+                    nextElement = kvp.Key;
+                    break;
+                }
+            }
+
+            while (firstCityKeyDict.ContainsKey(nextElement))
+            {
+                res.Add(nextElement, firstCityKeyDict[nextElement]);
+                nextElement = firstCityKeyDict[nextElement];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ TestsForCityChainClass.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using CityChain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityChain
+{
+    [TestFixture]
+    public class TestsForCityChainClass
+    {
+        CityChain cityChain;
+        [SetUp]
+        public void SetUp()
+        {
+            cityChain = new CityChain();
+        }
+
+        [Test]
+        public void TestCityChain()
+        {
+            string[,] cities = new string[5, 2] { { "Bern", "Berlin" }, { "Paris", "New York" }, { "Moscow", "Paris" }, { "New York", "Madrid" }, { "Madrid", "Bern" } };
+            Dictionary<string, string> res = cityChain.CreateChain(cities);
+            var sb = new StringBuilder();
+            foreach (KeyValuePair<string, string> pair in res)
+            {
+                sb.AppendFormat("{0} {1};", pair.Key, pair.Value);
+            }
+
+            Assert.AreEqual(sb.ToString(), "Moscow Paris;Paris New York;New York Madrid;Madrid Bern;Bern Berlin;");
+        }
+    }
+}
